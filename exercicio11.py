ccc3423c2c4d89aa3a2bfad9bfba2efdde26d2b0
Paulo José Cremasco Piva <cremascopaulo@gmail.com>
2021-08-09 01:04:50 -0300

    Add files via upload

---
+++ exercicio11.py
@@ -0,0 +1,7 @@
+primeiro = int(input('Primeiro '))
+segundo = int(input('Segundo '))
+terceiro = float(input('Terceiro '))
+
+print('a',(primeiro * 2)+(segundo / 2))
+print('b',(primeiro * 3)+ terceiro)
+print('c', terceiro ** 3)
